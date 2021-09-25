0119ee0e7e14a79bd920e234d236aec57394f082
formalDefault <hurtadoalejandro783@gmail.com>
2021-09-25 00:50:32 -0500

    sdgf

--- backend/database.js
+++ backend/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const URI = "mongodb+srv://root:ca6802ff14@crm.qdju1.mongodb.net/crm?retryWrites=true&w=majority"
 
 mongoose.connect(URI)
-    .then()
+    .then(console.log('conectado'))
     .catch(err => console.error(err));
 
 module.exports = mongoose; 
